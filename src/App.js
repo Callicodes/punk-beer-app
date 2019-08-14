e82c25bc0b5e4dc462ac6147ae6b90c2ced41743
Callicodes <callicodes@gmail.com>
2019-08-14 17:01:13 +0100

    Added A <Sidebar /> Component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main/Main";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <div className="App-main">
+          <div className="App-sidebar-wrapper">
+            <Sidebar />
+          </div>
+          <div className="App-content-wrapper">
+            <h1 className="App-main-title">Random Beers </h1>
+          </div>
+        </div>
+
         <Main title="randomBeers" />
       </div>
     );
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "./Sidebar.css";
+
+const Sidebar = () => {
+  return (
+    <div className="App-sidebar">
+      <ul className="sidebar-menu">
+        <li className="sidebar-menu__item">
+          <a href="/" className="is-active">
+            Random
+          </a>
+        </li>
+        <li className="sidebar-menu__item">
+          <a href="/top-rated">Top Rated</a>
+        </li>
+        <li className="sidebar-menu__item">
+          <a href="/coming-soon">Coming soon</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
