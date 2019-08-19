b5fe069bc6dbcb20a0a420395871bc87978c791f
Callicodes <callicodes@gmail.com>
2019-08-19 16:24:39 +0100

    <Styling Added>

--- src/components/BeerThumb/BeerThumb.js
+++ src/components/BeerThumb/BeerThumb.js
@@ -1,22 +1,22 @@
 import React from "react";
+import Styles from "styled-components";
 import { Link } from "react-router-dom";
-// import "./BeerThumb.css";
+import "./BeerThumb.css";
 
-const BeerThumb = props => {
-  return (
-    <div className="rmdb-beerthumb">
-      <p>{props.beerName}</p>
-      {props.clickable ? (
-        <Link
-          to={{ pathname: `/${props.beerId}`, beerName: `${props.beerName}` }}
-        >
-          <img clasName="bdb-beerimg" src={props.image} alt="beerthumb" />
-        </Link>
-      ) : (
-        <img src={props.image} alt="beerthumb" />
-      )}
-    </div>
-  );
-};
+const BeerThumb = props => (
+  <div className="bdb-beerthumb">
+    <p>{props.beerName}</p>
+    {props.clickable ? (
+      <Link
+        to={{ pathname: `/${props.beerId}`, beerName: `${props.beerName}` }}
+      >
+        <img className="bdb-beering" src={props.image} alt="beerthumb" />
+      </Link>
+    ) : (
+      <img src={props.image} alt="beerthumb" />
+    )}
+    <nbsp />
+  </div>
+);
 
 export default BeerThumb;
